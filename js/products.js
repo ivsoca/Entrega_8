7b0c6ffb44fc5c2ae8f5afd54b3728b0a24cfa85
guzb04 <guzmanbeyhaut@gmail.com>
2023-08-10 15:31:07 -0400

    saqué con fran la carpeta pages por problemas con el api
    había que linkear todo de nuevo y el api no podia sourcear las imagenes

---
+++ js/products.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const url = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+    const productList = document.querySelector("#product-list");
+  
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Error al hacer la solicitud.");
+        }
+        return response.json();
+      })
+      .then(data => {
+        const products = data.products;
+  
+         products.forEach(product => {
+          const productItem = document.createElement("div");
+          productItem.classList.add("product-item");
+  
+          productItem.innerHTML = 
+           `<h2>${product.name}</h2>
+            <p>${product.description}</p>
+            <p>Precio: ${product.cost} ${product.currency}</p>
+            <p>Vendidos: ${product.soldCount}</p>
+            <img src="${product.image}" alt="${product.name}">`
+          ;
+  
+          productList.appendChild(productItem);
+        });
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
+  });
