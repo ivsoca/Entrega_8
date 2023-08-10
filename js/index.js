318005804ac2a63d6977f2c04d3c581029869e4d
Guzb04 <guzmanbeyhaut@gmail.com>
2023-08-10 16:05:26 -0400

    no se borraron unas cosas, ahora deberia estar

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
-        window.location = "autos.html"
+        window.location = "products.html"
     });
     document.getElementById("juguetes").addEventListener("click", function() {
         localStorage.setItem("catID", 102);
-        window.location = "juguetes.html"
+        window.location = "products.html"
     });
     document.getElementById("muebles").addEventListener("click", function() {
         localStorage.setItem("catID", 103);
-        window.location = "muebles.html"
+        window.location = "products.html"
     });
 });
