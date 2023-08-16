a8abcd60edf36d767de64c61c86f74e79eb49575
falsofacu <facundotabarez0@gmail.com>
2023-08-16 09:50:26 -0300

    add specific alerts

--- js/login.js
+++ js/login.js
@@ -7,14 +7,39 @@ document.addEventListener("DOMContentLoaded", function () {
     let password = document.getElementById("passworduser").value;
     let emailHasAt = email.includes("@");
     let emailHasDot = email.includes(".");
+    let passwordIsLong = password.length > 6;
 
     if (email && password && emailHasAt && emailHasDot && password.length >= 6) {
       localStorage.setItem("email", email);
       window.location.href = "index.html";
     } else {
-      if (!emailHasAt) alert("El e-mail ingresado debe contener un arroba (@)");
-      else if (!emailHasDot) alert("El e-mail ingresado debe contener un punt (.)");
-      else alert("Por favor ingrese un correo y/o contraseña correcto(s)");
+
+      switch (false) {
+        case emailHasAt:
+          alert("El e-mail ingresado debe contener un arroba (@)");
+          break;
+        case emailHasDot:
+          alert("El e-mail ingresado debe contener un punto (.)");
+          break;
+        case passwordIsLong:
+          alert("La contraseña debe tener más de seis caracteres");
+          break;
+        default:
+          break;       
+      }
     }
   });
+});
+
+window.onload = function() {
+  let contenidoRecordado = localStorage.getItem('contenidoRecordado');
+  if (contenidoRecordado) {
+    document.getElementById('mailuser').value = contenidoRecordado;
+  }
+};
+
+document.getElementById('recordarme').addEventListener('click', function() {
+  var contenidoInput = document.getElementById('mailuser').value;
+
+  localStorage.setItem('contenidoRecordado', contenidoInput);
 });
