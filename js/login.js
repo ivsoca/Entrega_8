1d577bf51a23ef2d1a151560ddfb88114c6eee3a
falsofacu <facundotabarez0@gmail.com>
2023-08-16 09:38:10 -0300

    feat: add dot condition

--- js/login.js
+++ js/login.js
@@ -6,13 +6,15 @@ document.addEventListener("DOMContentLoaded", function () {
     let email = document.getElementById("mailuser").value;
     let password = document.getElementById("passworduser").value;
     let emailHasAt = email.includes("@");
-    // let emailHasDot = email.includes(".");
+    let emailHasDot = email.includes(".");
 
-    if (email && password && emailHasAt === true && password.length >= 6) {
+    if (email && password && emailHasAt && emailHasDot && password.length >= 6) {
       localStorage.setItem("email", email);
       window.location.href = "index.html";
     } else {
-      alert("Por favor ingrese un correo y/o contraseña correcto(s)");
+      if (!emailHasAt) alert("El e-mail ingresado debe contener un arroba (@)");
+      else if (!emailHasDot) alert("El e-mail ingresado debe contener un punt (.)");
+      else alert("Por favor ingrese un correo y/o contraseña correcto(s)");
     }
   });
 });
