af90b209380a88ce2cf0b0a38cb7e7e7ac478dbc
josefinamartinezdelpuerto <josefina2101996@gmail.com>
2023-09-11 19:46:57 -0300

    Add: traer comentarios del json. (falta css)

--- js/product-info.js
+++ js/product-info.js
@@ -1,8 +1,5 @@
 //Se genera un EventListenner para que al seleccionar la card haga varias funciones.
-document.addEventListener("DOMContentLoaded", () => {
-  //Se crean const que traen info del localStorage(producto que se clickeo y su nombre de categoria)
-  const product = JSON.parse(localStorage.getItem("productoClickeado"));
-  const productCategoryName = localStorage.getItem("catName");
+const cargarInfoProducto = (product, productCategoryName) => {
   //Se crean constantes para traer los elementos por su id del html
   const nombreProduct = document.getElementById("nombreProduct");
   const precioProduct = document.getElementById("precioProducto");
@@ -31,5 +28,57 @@ document.addEventListener("DOMContentLoaded", () => {
     listImageProduct.src = `/img/prod${product.id}_${i}.jpg`;
     carruselProduct.appendChild(listImageProduct);
   }
-});
+};
 
+const cargarComentariosProducto = async (product) => {
+  if (!product.id) return console.error("the product doesn't have an id");
+  const baseCommentUrl = `https://raw.githubusercontent.com/JaPCeibal/emercado-api/main/products_comments/${product.id}.json`;
+  const response = await fetch(baseCommentUrl);
+  if (!response.ok)
+    return console.error("Something went wrong when retrieving the comments");
+  const comments = await response.json();
+  comments.forEach((comment) => {
+    const divComment = document.createElement("div");
+    divComment.classList.add("wrapper-comentario");
+    const comentarioHeader = document.createElement("p");
+    comentarioHeader.classList.add("headerComentario");
+    divComment.appendChild(comentarioHeader);
+    const user = document.createElement("span");
+    const dateTime = document.createElement("span");
+    const description = document.createElement("span");
+
+    const userText = document.createTextNode(comment.user);
+    const dateTimeText = document.createTextNode(comment.dateTime);
+    const descriptionText = document.createTextNode(comment.description);
+    const scoreStar = document.createElement("span");
+
+    for (let i = 0; i < 5; i++) {
+      if (i < comment.score) {
+        const scoreStarText = document.createTextNode("★");
+        scoreStar.appendChild(scoreStarText);
+      } else {
+        const scoreStarText = document.createTextNode("☆");
+        scoreStar.appendChild(scoreStarText);
+      }
+    }
+
+    user.appendChild(userText);
+    dateTime.appendChild(dateTimeText);
+    description.appendChild(descriptionText);
+
+    comentarioHeader.appendChild(user);
+    comentarioHeader.appendChild(dateTime);
+    comentarioHeader.appendChild(scoreStar);
+    divComment.appendChild(descriptionText);
+
+    document.getElementById("comentarios").appendChild(divComment);
+  });
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+  //Se crean const que traen info del localStorage(producto que se clickeo y su nombre de categoria)
+  const product = JSON.parse(localStorage.getItem("productoClickeado"));
+  const productCategoryName = localStorage.getItem("catName");
+  cargarInfoProducto(product, productCategoryName);
+  await cargarComentariosProducto(product);
+});
