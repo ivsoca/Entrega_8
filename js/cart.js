aa548843237e6842690e33416f807d3b0e40b892
josefinamartinezdelpuerto <josefina2101996@gmail.com>
2023-10-02 09:44:50 -0300

    fix/ banner cortado

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-// Crear elemento nav que contiene todo el HTML de la caja del carrito de compras
-const cartNavElement = document.createElement("li");
-cartNavElement.innerHTML = `
+  // Crear elemento nav que contiene todo el HTML de la caja del carrito de compras
+  const cartNavElement = document.createElement("li");
+  cartNavElement.innerHTML = `
     <div class="container">
         <div class="btn-menu">
             <label for="btn-menu" class=""nav-item"><i class="fa-solid fa-cart-shopping" style="color: #ffd6ff;"></i></label>    
@@ -27,12 +26,98 @@ cartNavElement.innerHTML = `
         </div>
     </div>
 `;
-cartNavElement.classList.add("nav-item");
-cartNavElement.id = "login-nav-li";
+  cartNavElement.classList.add("nav-item");
+  cartNavElement.id = "login-nav-li";
 
-//* Agregar elemento nav a navbar
-const navbar = document.getElementById("navlist");
-navbar.appendChild(cartNavElement);
-
+  //* Agregar elemento nav a navbar
+  const navbar = document.getElementById("navlist");
+  navbar.appendChild(cartNavElement);
 });
 
+// Definir una variable para almacenar los productos en el carrito
+const carrito = [];
+
+// Función para agregar un producto al carrito
+function agregarAlCarrito(producto) {
+  carrito.push(producto);
+}
+
+// Función para eliminar un producto del carrito
+function eliminarDelCarrito(idProducto) {
+  const indice = carrito.findIndex((producto) => producto.id === idProducto);
+  if (indice !== -1) {
+    carrito.splice(indice, 1);
+  }
+}
+
+// Función para calcular el total del carrito
+function calcularTotalCarrito() {
+  let total = 0;
+  carrito.forEach((producto) => {
+    total += producto.precio;
+  });
+  return total;
+}
+
+// Función para actualizar la vista del carrito
+function actualizarCarrito() {
+  // Actualizar la lista de productos en el carrito
+  const listaProductosCarrito = document.getElementById("lista-productos");
+  listaProductosCarrito.innerHTML = "";
+  carrito.forEach((producto) => {
+    const itemCarrito = document.createElement("li");
+    itemCarrito.textContent = `${producto.nombre} - $${producto.precio.toFixed(
+      2
+    )}`;
+    const botonEliminar = document.createElement("button");
+    botonEliminar.textContent = "Eliminar";
+    botonEliminar.classList.add("eliminar-producto");
+    botonEliminar.setAttribute("data-id", producto.id);
+    itemCarrito.appendChild(botonEliminar);
+    listaProductosCarrito.appendChild(itemCarrito);
+  });
+
+  // Actualizar el total del carrito
+  const totalCarrito = document.getElementById("total-carrito");
+  totalCarrito.textContent = `$${calcularTotalCarrito().toFixed(2)}`;
+}
+
+// Eventos para botones y acciones del carrito
+document
+  .getElementById("lista-productos-disponibles")
+  .addEventListener("click", function (event) {
+    if (event.target.classList.contains("boton-producto")) {
+      const idProducto = event.target.getAttribute("data-id");
+      // Aquí debes obtener el producto correspondiente desde tu base de datos o fuente de datos
+      const producto = obtenerProductoPorId(idProducto);
+      if (producto) {
+        agregarAlCarrito(producto);
+        actualizarCarrito();
+      }
+    }
+  });
+
+document
+  .getElementById("lista-productos")
+  .addEventListener("click", function (event) {
+    if (event.target.classList.contains("eliminar-producto")) {
+      const idProducto = event.target.getAttribute("data-id");
+      eliminarDelCarrito(idProducto);
+      actualizarCarrito();
+    }
+  });
+
+document
+  .getElementById("vaciar-carrito")
+  .addEventListener("click", function () {
+    // Vaciar el carrito
+    carrito.length = 0;
+    actualizarCarrito();
+  });
+
+document
+  .getElementById("realizar-pedido")
+  .addEventListener("click", function () {
+    // Lógica para realizar un pedido (puede implicar enviar la información del carrito al servidor)
+    // Aquí puedes implementar la funcionalidad adicional que necesites para procesar el pedido
+  });
